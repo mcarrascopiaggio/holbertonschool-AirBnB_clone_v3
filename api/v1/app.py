ac74a953d5b2a4c89ec9c2e86368eda65f2f935d
mcarrascopiaggio <mcarrascopiaggio@gmail.com>
2022-09-08 13:21:51 -0700

    fix pep8

--- api/v1/app.py
+++ api/v1/app.py
@@ -1,4 +1,4 @@
-#/usr/bin/python3
+#!/usr/bin/python3
 """app pycode"""
 
 from api.v1.views import app_views
@@ -9,13 +9,14 @@ from os import getenv
 app = Flask(__name__)
 app.register_blueprint(app_views, url_prefix="/api/v1")
 
+
 @app.teardown_appcontext
 def close_session(self):
     """close session"""
     storage.close()
 
-if __name__ == "__main__":
 
+if __name__ == "__main__":
 	host = getenv("HBNB_API_HOST")
 	port = getenv("HBNB_API_PORT")
 
