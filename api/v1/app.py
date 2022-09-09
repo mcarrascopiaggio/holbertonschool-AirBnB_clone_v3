5fdb99c785b0bfbe59f733dcfdfd3f5a0ab7a740
root <mcarrascopiaggio@gmail.com>
2022-09-08 18:14:03 -0700

    try with another app

--- api/v1/app.py
+++ api/v1/app.py
@@ -1,39 +1,34 @@
 #!/usr/bin/python3
 """
-Your first endpoint (route) will be to return the status of your API
-line 3
-line 4
+API instance
 """
-
-from flask import Flask
+from unicodedata import category
+from flask import Flask, jsonify, make_response
 from models import storage
 from api.v1.views import app_views
 from os import getenv
 
 app = Flask(__name__)
-app.register_blueprint(app_views, url_prefix="/api/v1")
+app.register_blueprint(app_views)
 
 
 @app.teardown_appcontext
-def close_session(self):
-    """close session"""
+def storage_close(self):
+    """
+    Calls storage.close()
+    """
     storage.close()
 
 
 @app.errorhandler(404)
-def error_404(error):
-    """error 404 page not found"""
-    error = {"error": "Not found"}
-    return (jsonify(error), 404)
+def notfound_404(error):
+    """
+    Retrieves the number of each objects by type.
+    """
+    return make_response(jsonify({"error": "Not found"}), 404)
 
 
 if __name__ == "__main__":
-    host = getenv("HBNB_API_HOST")
-    port = getenv("HBNB_API_PORT")
-
-    if not host:
-        host = "0.0.0.0"
-
-    if not port:
-        port = "5000"
+    host = getenv("HBNB_API_HOST", "0.0.0.0")
+    port = getenv("HBNB_API_PORT", "5000")
     app.run(host=host, port=port, threaded=True)
