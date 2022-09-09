70140185a530f9b81a038e976f53ead62472cd95
root <mcarrascopiaggio@gmail.com>
2022-09-08 18:32:10 -0700

    try some changes with juanma

--- api/v1/app.py
+++ api/v1/app.py
@@ -11,7 +11,8 @@ from api.v1.views import app_views
 from os import getenv
 
 app = Flask(__name__)
-app.register_blueprint(app_views, url_prefix="/api/v1")
+app.register_blueprint(app_views)
+# app.register_blueprint(app_views, url_prefix="/api/v1")
 
 
 @app.teardown_appcontext
