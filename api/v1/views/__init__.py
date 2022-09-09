b854270173c1c1d456d3b14c7a9c9940f3171ecd
root <mcarrascopiaggio@gmail.com>
2022-09-08 18:34:52 -0700

    go back

--- api/v1/views/__init__.py
+++ api/v1/views/__init__.py
@@ -5,6 +5,6 @@ from api.v1.views.index import *
 from flask import Flask, Blueprint
 
 app = Flask(__name__)
-app_views = Blueprint("app_views", __name__, url_prefix="/api/v1")
-# app_views = Blueprint("app_views", __name__)
-# app.register_blueprint(app_views, url_prefix="/api/v1")
+# app_views = Blueprint("app_views", __name__, url_prefix="/api/v1")
+app_views = Blueprint("app_views", __name__)
+app.register_blueprint(app_views, url_prefix="/api/v1")
