e708b548d2b56e8cab0def1c316d1d3562949395
root <mcarrascopiaggio@gmail.com>
2022-09-09 05:22:48 -0700

    try another import way

--- api/v1/app.py
+++ api/v1/app.py
@@ -5,14 +5,20 @@ line 3
 line 4
 """
 
-from flask import Flask
+from flask import Flask, jsonify, make_response
 from models import storage
 from api.v1.views import app_views
 from os import getenv
 
 app = Flask(__name__)
-# app.register_blueprint(app_views)
-app.register_blueprint(app_views, url_prefix="/api/v1")
+app.register_blueprint(app_views)
+
+# from flask import Flask
+# from models import storage
+# from api.v1.views import app_views
+# from os import getenv
+# app = Flask(__name__)
+# app.register_blueprint(app_views, url_prefix="/api/v1")
 
 
 @app.teardown_appcontext
--- api/v1/views/__init__.py
+++ api/v1/views/__init__.py
@@ -1,10 +1,13 @@
 #!/usr/bin/python3
 """init"""
 
-from api.v1.views.index import *
-from flask import Flask, Blueprint
+# from api.v1.views.index import *
+# from flask import Flask, Blueprint
+# app = Flask(__name__)
+# app_views = Blueprint("app_views", __name__)
+# app.register_blueprint(app_views, url_prefix="/api/v1")
 
-app = Flask(__name__)
-# app_views = Blueprint("app_views", __name__, url_prefix="/api/v1")
-app_views = Blueprint("app_views", __name__)
-app.register_blueprint(app_views, url_prefix="/api/v1")
+from flask import Blueprint
+from api.v1.views.index import *
+
+app_views = Blueprint("app_views", _name_, url_prefix="/api/v1")
