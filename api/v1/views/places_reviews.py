bd39431d8cfbb57f6529f9dbf8036221491c819f
root <mcarrascopiaggio@gmail.com>
2022-09-11 07:49:31 -0700

    add first method GET

---
+++ api/v1/views/places_reviews.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python3
+"""
+Create a new view for Review
+object that handles all default RESTFul API actions
+"""
+from flask import Flask, jsonify, abort, request
+from api.v1.views import app_views
+from models.place import Place
+from models.review import Review
+from models.user import User
+from models import storage
+
+
+@app_views.route('/places/<string:place_id>/reviews', methods=['GET'],
+                 strict_slashes=False)
+def get_review(place_id):
+    """
+    Retrieves the list of all Review objects of a Place:
+    GET /api/v1/places/<place_id>/reviews
+    """
+    list_all_reviews = []
+    place = storage.get("Place", place_id)
+    if not place:
+        abort(404)
+    for review in place.reviews:
+        list_all_reviews.append(review.to_dict())
+
+    return jsonify(list_review)
