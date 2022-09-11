36d3478d87936ae0cfc136f7adb168c3ab0bc808
root <mcarrascopiaggio@gmail.com>
2022-09-11 07:21:18 -0700

    add comillas to third method

--- api/v1/views/places.py
+++ api/v1/views/places.py
@@ -40,19 +40,20 @@ def get_place(place_id):
         return jsonify(theplace.to_dict())
 
 
-@app_views.route("/places/<place_id>", methods=["DELETE"],
+@app_views.route("/places/<place_id>",
+                 methods=["DELETE"],
                  strict_slashes=False)
 def delete_place(place_id):
-    """Deletes a amenity instance"""
-
+    """
+    deletes a Place object
+    """
     place = storage.get("Place", place_id)
-
     if place is None:
         abort(404)
     else:
-        place.delete()
+        storage.delete(place)
         storage.save()
-        return make_response(jsonify({}), 200)
+        return jsonify({}), 200
 
 
 @app_views.route("/cities/<city_id>/places", methods=["POST"],
